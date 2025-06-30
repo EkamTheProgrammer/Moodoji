566243d5e4203681ea28e3e5a01f18d41c7980f6
EkamTheProgrammer <earthekam@gmail.com>
2025-06-30 13:28:52 -0700

    Initial Commit

---
+++ Moodoji/MoodojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodojiApp.swift
+//  Moodoji
+//
+//  Created by Ekampreet Singh on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
